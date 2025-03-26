b16fc50063f53aa658e1184a3af37e9b19528dcc
Glitchyi <advaith@glitchy.systems>
2025-03-26 12:26:17 +0530

    skibidi

--- utils/webpage-summary.py
+++ utils/webpage-summary.py
@@ -8,6 +8,8 @@ image = modal.Image.debian_slim().pip_install("flask","langchain-groq","langchai
 @app.function(image=image, secrets=[modal.Secret.from_name("GROQ_API_KEY")])
 @modal.wsgi_app()
 def flask_app():
+    from os import environ
+    from flask import Flask, request, jsonify
     from langchain_groq import ChatGroq
     from langchain_core.vectorstores import InMemoryVectorStore
     from langchain_huggingface import HuggingFaceEmbeddings
@@ -19,14 +21,15 @@ def flask_app():
 
     web_app = Flask(__name__)
     
+    groq_api_key = environ.get("GROQ_API_KEY")
     
-    llm = ChatGroq(model='llama-3.3-70b-versatile', api_key=os.environ["GROQ_API_KEY"])
+    llm = ChatGroq(model='llama-3.3-70b-versatile', api_key=groq_api_key)
+
+
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vector_store = InMemoryVectorStore(embeddings)
     
 
-    # url = "https://www.writeoutloud.net/blogs/"
-    
     prompt = PromptTemplate.from_template("""You will be provided with the contents of a webpage.
     Your task is to analyze the contents, summarize its contents in no more than 200 words, and optimize the summary for SEO.
     <context>
@@ -35,17 +38,17 @@ def flask_app():
     Question={input}
     Follow these steps:
         1. Summarize the primary content of the website accurately.
+        2. Make the summarised content very short fitting in a SINGLE LINE.
         2. Optimize the summary for SEO, incorporating relevant keywords and phrases naturally.
         3. Ensure the summary is concise, clear, and engaging for SEO bots.
-        4. Generate ONLY the summary, no need for any additional statements
-        5. The summary should ONLY be in a simple HTML format with proper structure fit for SEO
-        6. The output html SHOULD have head section body section and proper meta tags
+        4. Generate ONLY the summary, no need for any additional statements.
+        5. The summary should ONLY be in a simple HTML format with proper structure fit for SEO.
+        6. The output html SHOULD ONLY have meta tags.
     """)
 
 
     @web_app.post("/")
     def home():
-        url = request.args.get('url')
         data = request.get_json()
         
         if not data or 'html' not in data:
@@ -57,31 +60,24 @@ def flask_app():
         # Print the received HTML content
         print("Received HTML content:")
         print(docs)
-        docs_text = "".join(d.page_content for d in docs)
-
-        # Populate the system prompt with the retrieved context
-        system_prompt_fmt = prompt.format(context=docs_text)
-#         loader = WebBaseLoader(
-#             web_paths=(url,),
-#         )
-#         html = loader.load()
         
-#         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-#         docs = text_splitter.split_documents(html)
-#         _ = vector_store.add_documents(documents=docs)
-#         retriever = vector_store.as_retriever()
+        html = [Document(page_content=docs)]
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        docs = text_splitter.split_documents(html)
+        _ = vector_store.add_documents(documents=docs)
+        retriever = vector_store.as_retriever()
 
-#         rag_chain = (
-#                 {"context": retriever, "input": RunnablePassthrough()}
-#                 | prompt
-#                 | llm
-#                 | StrOutputParser()
-#             )
+        rag_chain = (
+                {"context": retriever, "input": RunnablePassthrough()}
+                | prompt
+                | llm
+                | StrOutputParser()
+            )
 
-#         summary = rag_chain.invoke("Summarize")
+        summary = rag_chain.invoke("Summarize")
 
-#         return summary
-
+        return summary
 
     return web_app
 
